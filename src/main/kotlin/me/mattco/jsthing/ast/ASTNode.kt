62cc7652324ecb0878929d916f05195c890662c3
Matthew Olsson <matthewcolsson@gmail.com>
2020-10-20 12:30:31 -0700

    SS: Give most SS a default implementation

--- src/main/kotlin/me/mattco/jsthing/ast/ASTNode.kt
+++ src/main/kotlin/me/mattco/jsthing/ast/ASTNode.kt
@@ -52,15 +52,13 @@ abstract class ASTNode(private val children: List<ASTNode> = emptyList()) {
 
     open fun assignmentTargetType(): AssignmentTargetType {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "assignmentTargetType, and cannot be delegated")
+            return AssignmentTargetType.Invalid
         return children[0].assignmentTargetType()
     }
 
     open fun boundNames(): List<String> {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "boundNames, and cannot be delegated")
+            return emptyList()
         return children[0].boundNames()
     }
 
@@ -85,29 +83,25 @@ abstract class ASTNode(private val children: List<ASTNode> = emptyList()) {
 
     open fun containsDuplicateLabels(labelSet: Set<String>): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "containsDuplicateLabels, and cannot be delegated")
+            return false
         return children[0].containsDuplicateLabels(labelSet)
     }
 
     open fun containsExpression(): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "containsExpression, and cannot be delegated")
+            return false
         return children[0].containsExpression()
     }
 
     open fun containsUndefinedBreakTarget(labelSet: Set<String>): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "containsUndefinedBreakTarget, and cannot be delegated")
+            return false
         return children[0].containsUndefinedBreakTarget(labelSet)
     }
 
     open fun containsUndefinedContinueTarget(iterationSet: Set<String>, labelSet: Set<String>): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "containsUndefinedContinueTarget, and cannot be delegated")
+            return false
         return children[0].containsUndefinedContinueTarget(iterationSet, labelSet)
     }
 
@@ -127,113 +121,98 @@ abstract class ASTNode(private val children: List<ASTNode> = emptyList()) {
 
     open fun declarationPart(): ASTNode {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation " +
-                "), and cannot be delegated")
+            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
+                "declarationPart, and cannot be delegated")
         return children[0].declarationPart()
     }
 
     open fun hasInitializer(): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation " +
-                ":, and cannot be delegated")
+            return false
         return children[0].hasInitializer()
     }
 
     open fun hasName(): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "Boolean, and cannot be delegated")
+            return false
         return children[0].hasName()
     }
 
     open fun isComputedPropertyKey(): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "isComputedPropertyKey, and cannot be delegated")
+            return false
         return children[0].isComputedPropertyKey()
     }
 
     open fun isConstantDeclaration(): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "isConstantDeclaration, and cannot be delegated")
+            return false
         return children[0].isConstantDeclaration()
     }
 
     open fun isDestructuring(): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation " +
-                "), and cannot be delegated")
+            return false
         return children[0].isDestructuring()
     }
 
     open fun isFunctionDefinition(): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "isFunctionDefinition, and cannot be delegated")
+            return false
         return children[0].isFunctionDefinition()
     }
 
     open fun isIdentifierRef(): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation " +
-                "), and cannot be delegated")
+            return false
         return children[0].isIdentifierRef()
     }
 
     open fun isLabelledFunction(): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "isLabelledFunction, and cannot be delegated")
+            return false
         return children[0].isLabelledFunction()
     }
 
     open fun isSimpleParameterList(): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "isSimpleParameterList, and cannot be delegated")
+            return false
         return children[0].isSimpleParameterList()
     }
 
     open fun isStrict(): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "Boolean, and cannot be delegated")
+            return false
         return children[0].isStrict()
     }
 
     open fun isValidRegularExpressionLiteral(): Boolean {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "isValidRegularExpressionLiteral, and cannot be delegated")
+            return false
         return children[0].isValidRegularExpressionLiteral()
     }
 
     open fun lexicallyDeclaredNames(): List<String> {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "lexicallyDeclaredNames, and cannot be delegated")
+            return emptyList()
         return children[0].lexicallyDeclaredNames()
     }
 
     open fun lexicallyScopedDeclarations(): List<ASTNode> {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "lexicallyScopedDeclarations, and cannot be delegated")
+            return emptyList()
         return children[0].lexicallyScopedDeclarations()
     }
 
     open fun propertyNameList(): List<String> {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation " +
-                "(, and cannot be delegated")
+            return emptyList()
         return children[0].propertyNameList()
     }
 
     open fun propName(): String? {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "String, and cannot be delegated")
+            return null
         return children[0].propName()
     }
 
@@ -246,50 +225,43 @@ abstract class ASTNode(private val children: List<ASTNode> = emptyList()) {
 
     open fun templateStrings(): List<String> {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation " +
-                "), and cannot be delegated")
+            return emptyList()
         return children[0].templateStrings()
     }
 
     open fun topLevelLexicallyDeclaredNames(): List<String> {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "topLevelLexicallyDeclaredNames, and cannot be delegated")
+            return emptyList()
         return children[0].topLevelLexicallyDeclaredNames()
     }
 
     open fun topLevelLexicallyScopedDeclarations(): List<ASTNode> {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "topLevelLexicallyScopedDeclarations, and cannot be delegated")
+            return emptyList()
         return children[0].topLevelLexicallyScopedDeclarations()
     }
 
     open fun topLevelVarDeclaredNames(): List<String> {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "topLevelVarDeclaredNames, and cannot be delegated")
+            return emptyList()
         return children[0].topLevelVarDeclaredNames()
     }
 
     open fun topLevelVarScopedDeclarations(): List<ASTNode> {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "topLevelVarScopedDeclarations, and cannot be delegated")
+            return emptyList()
         return children[0].topLevelVarScopedDeclarations()
     }
 
     open fun varDeclaredNames(): List<String> {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation " +
-                "(, and cannot be delegated")
+            return emptyList()
         return children[0].varDeclaredNames()
     }
 
     open fun varScopedDeclarations(): List<ASTNode> {
         if (children.size != 1)
-            throw Error("Node ${this::class.java.simpleName} has no implementation for " +
-                "varScopedDeclarations, and cannot be delegated")
+            return emptyList()
         return children[0].varScopedDeclarations()
     }
 }
