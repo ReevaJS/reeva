b7fefb578a2c67d27bc390db82b4f402504505eb
Matthew Olsson <matthewcolsson@gmail.com>
2020-10-21 00:00:55 -0700

    Utils: Add convenience value conversion helpers

--- src/main/kotlin/me/mattco/jsthing/utils/extensions.kt
+++ src/main/kotlin/me/mattco/jsthing/utils/extensions.kt
@@ -1,6 +1,10 @@
 package me.mattco.jsthing.utils
 
 import me.mattco.jsthing.runtime.values.nonprimitives.objects.PropertyKey
+import me.mattco.jsthing.runtime.values.primitives.JSFalse
+import me.mattco.jsthing.runtime.values.primitives.JSNumber
+import me.mattco.jsthing.runtime.values.primitives.JSString
+import me.mattco.jsthing.runtime.values.primitives.JSTrue
 
 fun Iterable<Boolean>.all() = this.all { it }
 
@@ -15,3 +19,9 @@ fun stringBuilder(builder: StringBuilder.() -> Unit) = StringBuilder().apply(bui
 fun StringBuilder.newline() = append("\n")
 
 fun String.key() = PropertyKey(this)
+
+fun Boolean.toValue() = if (this) JSTrue else JSFalse
+
+fun String.toValue() = JSString(this)
+
+fun Number.toValue() = JSNumber(this.toDouble())
