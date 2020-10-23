6d04ccbd3977b6f84b3978d7fb472be0a3ff6f67
Matthew Olsson <matthewcolsson@gmail.com>
2020-10-22 17:25:29 -0700

    JSObjectCtor: Implement call function

--- src/main/kotlin/me/mattco/reeva/runtime/values/objects/JSObjectCtor.kt
+++ src/main/kotlin/me/mattco/reeva/runtime/values/objects/JSObjectCtor.kt
@@ -1,15 +1,21 @@
 package me.mattco.reeva.runtime.values.objects
 
+import me.mattco.reeva.runtime.Operations
 import me.mattco.reeva.runtime.Realm
 import me.mattco.reeva.runtime.values.JSValue
 import me.mattco.reeva.runtime.values.functions.JSNativeFunction
+import me.mattco.reeva.utils.JSArguments
+import me.mattco.reeva.utils.argument
 
 class JSObjectCtor private constructor(realm: Realm) : JSNativeFunction(realm, "ObjectConstructor", 1) {
-    override fun call(thisValue: JSValue, arguments: List<JSValue>): JSValue {
-        TODO("Not yet implemented")
+    override fun call(thisValue: JSValue, arguments: JSArguments): JSValue {
+        val value = arguments.argument(0)
+        if (value.isUndefined || value.isNull)
+            return JSObject.create(realm)
+        return Operations.toObject(value)
     }
 
-    override fun construct(arguments: List<JSValue>, newTarget: JSValue): JSValue {
+    override fun construct(arguments: JSArguments, newTarget: JSValue): JSValue {
         TODO("Not yet implemented")
     }
 
