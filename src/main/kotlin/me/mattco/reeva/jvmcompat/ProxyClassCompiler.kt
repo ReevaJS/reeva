3c1562fe81d89afa558d44fb4448687735252305
Matthew Olsson <matthewcolsson@gmail.com>
2020-11-16 11:40:11 -0800

    JVMCompat: Add support for delegating to super methods
    If a jsDelegate does not have an own property for a particular method,
    that method will delegate to the super method of its base class, or, if
    no such method exists, a default method implementation of an interface.
    The default interface method delegation also works for Kotlin default
    interface methods, which are implemented using static child classes
    instead of JVM default methods

--- src/main/kotlin/me/mattco/reeva/jvmcompat/ProxyClassCompiler.kt
+++ src/main/kotlin/me/mattco/reeva/jvmcompat/ProxyClassCompiler.kt
@@ -2,7 +2,9 @@ package me.mattco.reeva.jvmcompat
 
 import codes.som.anthony.koffee.assembleClass
 import codes.som.anthony.koffee.insns.jvm.*
+import codes.som.anthony.koffee.insns.sugar.JumpCondition
 import codes.som.anthony.koffee.insns.sugar.construct
+import codes.som.anthony.koffee.insns.sugar.ifElseStatement
 import codes.som.anthony.koffee.modifiers.Modifiers
 import codes.som.anthony.koffee.modifiers.public
 import me.mattco.reeva.Reeva
@@ -15,6 +17,7 @@ import me.mattco.reeva.runtime.objects.JSObject
 import me.mattco.reeva.runtime.objects.PropertyKey
 import me.mattco.reeva.utils.expect
 import org.objectweb.asm.ClassWriter
+import org.objectweb.asm.Type
 import java.io.File
 import java.io.FileOutputStream
 import java.lang.reflect.Modifier
@@ -47,14 +50,6 @@ class ProxyClassCompiler : Compiler() {
         val classNode = assembleClass(public, className, superName = superName, interfaces = interfaces.toList() + listOf(JVMProxyMarker::class.java)) {
             field(private + final, "jsDelegate", JSObject::class)
 
-            classes.forEach {
-                it.declaredFields.filterNot { field ->
-                    Modifier.isStatic(field.modifiers) || Modifier.isFinal(field.modifiers)
-                }.forEach { field ->
-                    field(Modifiers(field.modifiers and Modifier.ABSTRACT.inv()), field.name, field.type)
-                }
-            }
-
             baseClass?.declaredConstructors?.forEach { ctor ->
                 method(Modifiers(ctor.modifiers), "<init>", void, JSObject::class, *ctor.parameterTypes) {
                     aload_0
@@ -69,54 +64,101 @@ class ProxyClassCompiler : Compiler() {
                 }
             }
 
-            classes.forEach {
-                it.declaredMethods.filterNot { method ->
+            classes.forEach { clazz ->
+                clazz.declaredMethods.filterNot { method ->
                     Modifier.isStatic(method.modifiers) || Modifier.isFinal(method.modifiers)
                 }.forEach { method ->
                     method(Modifiers(method.modifiers and Modifier.ABSTRACT.inv()), method.name, method.returnType, *method.parameterTypes) {
-                        aload_0
-                        getfield(className, "jsDelegate", JSObject::class)
-                        construct(PropertyKey::class, String::class) {
-                            ldc(method.name)
-                        }
-
-                        construct(ArrayList::class)
-
-                        for (i in 1..method.parameterCount) {
-                            dup
-                            loadRealm()
-                            aload(i)
-                            invokestatic(JVMValueMapper::class, "jvmToJS", JSValue::class, Realm::class, Any::class)
-                            invokeinterface(List::class, "add", Boolean::class, Any::class)
-                            pop
-                        }
-
-                        operation("invoke", JSValue::class, JSValue::class, PropertyKey::class, List::class)
-                        if (method.returnType == Void::class.java) {
-                            _return
-                            return@method
-                        }
-
                         data class RetInfo(val wrapperType: String, val primitiveMethod: String?, val instr: () -> Unit)
 
-                        val (wrapperType, primitiveMethod, retInstr) = when (method.returnType) {
-                            Float::class.java -> RetInfo("java/lang/Float", "floatValue") { freturn }
-                            Double::class.java -> RetInfo("java/lang/Double", "doubleValue") { dreturn }
-                            Long::class.java -> RetInfo("java/lang/Long", "longValue") { lreturn }
-                            Byte::class.java -> RetInfo("java/lang/Byte", "byteValue") { ireturn }
-                            Short::class.java -> RetInfo("java/lang/Short", "shortValue") { ireturn }
-                            Char::class.java -> RetInfo("java/lang/Char", "charValue") { ireturn }
-                            Int::class.java -> RetInfo("java/lang/Integer", "intValue") { ireturn }
-                            else -> RetInfo(method.returnType.typeName, null) { areturn }
+                        fun doReturn(coerce: Boolean = true) {
+                            val (wrapperType, primitiveMethod, retInstr) = when (method.returnType) {
+                                Float::class.java -> RetInfo("java/lang/Float", "floatValue") { freturn }
+                                Double::class.java -> RetInfo("java/lang/Double", "doubleValue") { dreturn }
+                                Long::class.java -> RetInfo("java/lang/Long", "longValue") { lreturn }
+                                Byte::class.java -> RetInfo("java/lang/Byte", "byteValue") { ireturn }
+                                Short::class.java -> RetInfo("java/lang/Short", "shortValue") { ireturn }
+                                Char::class.java -> RetInfo("java/lang/Char", "charValue") { ireturn }
+                                Int::class.java -> RetInfo("java/lang/Integer", "intValue") { ireturn }
+                                else -> RetInfo(method.returnType.typeName, null) { areturn }
+                            }
+
+                            if (coerce) {
+                                ldc(coerceType(wrapperType))
+                                invokestatic(JVMValueMapper::class, "coerceValueToType", Any::class, JSValue::class, Class::class)
+
+                                checkcast(wrapperType)
+                                if (primitiveMethod != null)
+                                    invokevirtual(wrapperType, primitiveMethod, method.returnType)
+                            }
+
+                            retInstr()
                         }
 
-                        ldc(coerceType(wrapperType))
-                        invokestatic(JVMValueMapper::class, "coerceValueToType", Any::class, JSValue::class, Class::class)
+                        fun jsDelegation() {
+                            aload_0
+                            getfield(className, "jsDelegate", JSObject::class)
+                            construct(PropertyKey::class, String::class) {
+                                ldc(method.name)
+                            }
 
-                        checkcast(wrapperType)
-                        if (primitiveMethod != null)
-                            invokevirtual(wrapperType, primitiveMethod, method.returnType)
-                        retInstr()
+                            construct(ArrayList::class)
+
+                            for (i in 1..method.parameterCount) {
+                                dup
+                                loadRealm()
+                                aload(i)
+                                invokestatic(JVMValueMapper::class, "jvmToJS", JSValue::class, Realm::class, Any::class)
+                                invokeinterface(List::class, "add", Boolean::class, Any::class)
+                                pop
+                            }
+
+                            operation("invoke", JSValue::class, JSValue::class, PropertyKey::class, List::class)
+                            if (method.returnType == Void::class.java) {
+                                _return
+                            } else doReturn()
+                        }
+
+                        val defaultImpls = clazz.declaredClasses.firstOrNull {
+                            it.simpleName == "DefaultImpls" &&
+                                Modifier.isStatic(it.modifiers) &&
+                                Modifier.isPublic(it.modifiers) &&
+                                Modifier.isFinal(it.modifiers)
+                        }
+
+                        val methodDefaultImpl = defaultImpls?.declaredMethods?.firstOrNull {
+                            it.name == method.name && it.returnType == method.returnType &&
+                                Modifier.isStatic(it.modifiers) && Modifier.isPublic(it.modifiers) &&
+                                it.parameterCount >= 1 && it.parameterTypes[0].name == clazz.name
+                        }
+
+                        if (!Modifier.isAbstract(method.modifiers) || methodDefaultImpl != null) {
+                            aload_0
+                            getfield(className, "jsDelegate", JSObject::class)
+                            construct(PropertyKey::class, String::class) {
+                                ldc(method.name)
+                            }
+                            operation("hasOwnProperty", Boolean::class, JSValue::class, PropertyKey::class)
+                            ifElseStatement(JumpCondition.True) {
+                                ifBlock {
+                                    jsDelegation()
+                                }
+
+                                elseBlock {
+                                    aload_0
+                                    for (i in 1..method.parameterCount)
+                                        aload(i)
+                                    if (methodDefaultImpl != null) {
+                                        invokestatic(defaultImpls.name.replace('.', '/'), methodDefaultImpl.name, descriptor = Type.getMethodDescriptor(methodDefaultImpl))
+                                    } else {
+                                        invokevirtual(clazz.name.replace('.', '/'), method.name, descriptor = Type.getMethodDescriptor(method))
+                                    }
+                                    doReturn(coerce = false)
+                                }
+                            }
+                        } else {
+                            jsDelegation()
+                        }
                     }
                 }
             }
