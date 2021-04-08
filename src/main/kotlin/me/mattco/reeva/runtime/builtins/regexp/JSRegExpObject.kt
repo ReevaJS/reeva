922e186b5645014f0f0f0558ec30125c2f3d2a6c
Matthew Olsson <matthewcolsson@gmail.com>
2021-04-08 09:17:16 -0700

    Build: Update Joni

--- src/main/kotlin/me/mattco/reeva/runtime/builtins/regexp/JSRegExpObject.kt
+++ src/main/kotlin/me/mattco/reeva/runtime/builtins/regexp/JSRegExpObject.kt
@@ -10,7 +10,6 @@ import org.joni.Option
 import org.joni.Regex
 import org.joni.Syntax
 import org.joni.constants.MetaChar
-import org.joni.constants.SyntaxProperties.*
 import org.joni.exception.ErrorMessages
 import org.joni.exception.SyntaxException
 import org.joni.exception.ValueException
@@ -53,35 +52,40 @@ class JSRegExpObject private constructor(
         )
 
         private fun makeSyntax(flags: String): Syntax {
-            var op =
-                GNU_REGEX_OP or
-                    OP_QMARK_NON_GREEDY or
-                    OP_ESC_OCTAL3 or
-                    OP_ESC_X_HEX2 or
-                    OP_ESC_CONTROL_CHARS or
-                    OP_ESC_C_CONTROL or
-                    OP_DECIMAL_BACKREF or
-                    OP_ESC_D_DIGIT or
-                    OP_ESC_S_WHITE_SPACE or
-                    OP_ESC_W_WORD and
-                    OP_ESC_LTGT_WORD_BEGIN_END.inv()
+            // TODO: Joni was updated after this was written. Revisit and check
+            // the functionality of Syntax.ECMAScript
 
-            if (Flag.DotAll.char in flags)
-                op = op or OP_DOT_ANYCHAR
+//            var op =
+//                GNU_REGEX_OP or
+//                    OP_QMARK_NON_GREEDY or
+//                    OP_ESC_OCTAL3 or
+//                    OP_ESC_X_HEX2 or
+//                    OP_ESC_CONTROL_CHARS or
+//                    OP_ESC_C_CONTROL or
+//                    OP_DECIMAL_BACKREF or
+//                    OP_ESC_D_DIGIT or
+//                    OP_ESC_S_WHITE_SPACE or
+//                    OP_ESC_W_WORD and
+//                    OP_ESC_LTGT_WORD_BEGIN_END.inv()
+//
+//            if (Flag.DotAll.char in flags)
+//                op = op or OP_DOT_ANYCHAR
+//
+//            val op2 =
+//                CONTEXT_INDEP_ANCHORS or
+//                    CONTEXT_INDEP_REPEAT_OPS or
+//                    CONTEXT_INVALID_REPEAT_OPS or
+//                    ALLOW_INVALID_INTERVAL or
+//                    BACKSLASH_ESCAPE_IN_CC or
+//                    ALLOW_DOUBLE_RANGE_OP_IN_CC or
+//                    DIFFERENT_LEN_ALT_LOOK_BEHIND or
+//                    OP2_QMARK_LT_NAMED_GROUP
+//
+//            val behavior = DIFFERENT_LEN_ALT_LOOK_BEHIND
+//
+//            return Syntax("Reeva RegExp", op, op2, behavior, 0, metaCharTable)
 
-            val op2 =
-                CONTEXT_INDEP_ANCHORS or
-                    CONTEXT_INDEP_REPEAT_OPS or
-                    CONTEXT_INVALID_REPEAT_OPS or
-                    ALLOW_INVALID_INTERVAL or
-                    BACKSLASH_ESCAPE_IN_CC or
-                    ALLOW_DOUBLE_RANGE_OP_IN_CC or
-                    DIFFERENT_LEN_ALT_LOOK_BEHIND or
-                    OP2_QMARK_LT_NAMED_GROUP
-
-            val behavior = DIFFERENT_LEN_ALT_LOOK_BEHIND
-
-            return Syntax(op, op2, behavior, 0, metaCharTable)
+            return Syntax.ECMAScript
         }
 
         @ECMAImpl("N/A", name = "The [[RegExpMatcher]] Abstract Closure")
@@ -102,13 +106,13 @@ class JSRegExpObject private constructor(
                 return Regex(source.toByteArray(), 0, source.length, options, UTF8Encoding.INSTANCE, makeSyntax(flags))
             } catch (e: ValueException) {
                 when (e.message) {
-                    ErrorMessages.ERR_EMPTY_RANGE_IN_CHAR_CLASS -> Errors.RegExp.InvalidRange
-                    ErrorMessages.ERR_UPPER_SMALLER_THAN_LOWER_IN_REPEAT_RANGE -> Errors.RegExp.BackwardsBraceQuantifier
+                    ErrorMessages.EMPTY_RANGE_IN_CHAR_CLASS -> Errors.RegExp.InvalidRange
+                    ErrorMessages.UPPER_SMALLER_THAN_LOWER_IN_REPEAT_RANGE -> Errors.RegExp.BackwardsBraceQuantifier
                     else -> throw e
                 }.throwSyntaxError()
             } catch (e: SyntaxException) {
                 when (e.message) {
-                    ErrorMessages.ERR_END_PATTERN_AT_ESCAPE -> Errors.RegExp.BadEscape
+                    ErrorMessages.END_PATTERN_AT_ESCAPE -> Errors.RegExp.BadEscape
                     else -> throw e
                 }.throwSyntaxError()
             }
