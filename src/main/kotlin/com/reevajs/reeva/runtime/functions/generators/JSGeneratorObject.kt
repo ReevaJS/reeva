b0fe60c8a4fb0122adf719195dd09ddd205fcc38
Matthew Olsson <matthewcolsson@gmail.com>
2021-10-15 19:25:16 -0700

    Interpreter: Add basic generator support
    This add generator support to this Reeva rewrite branch. The primary
    difference between this implementation of generators and the last is
    that this implementation is mostly done in the Transformer. This has
    two advantages:
        - Less machinery and bloat in the Interpreter. All the Interpreter
          has to do is understand the (very simple) generator-specific
          opcodes.
        - IR is much easier to translate to JVM bytecode. Because
          everything is encoded directly in the IR (via a large switch
          statement) instead of shoehorned into the Interpreter,
          translating generators to JVM bytecode should be relatively
          painless.

--- src/main/kotlin/com/reevajs/reeva/runtime/functions/generators/JSGeneratorObject.kt
+++ src/main/kotlin/com/reevajs/reeva/runtime/functions/generators/JSGeneratorObject.kt
@@ -1,29 +1,65 @@
 package com.reevajs.reeva.runtime.functions.generators
 
 import com.reevajs.reeva.core.Realm
+import com.reevajs.reeva.core.ThrowException
 import com.reevajs.reeva.core.environment.EnvRecord
+import com.reevajs.reeva.core.lifecycle.Executable
+import com.reevajs.reeva.core.lifecycle.ExecutionResult
 import com.reevajs.reeva.interpreter.Interpreter
+import com.reevajs.reeva.runtime.JSValue
+import com.reevajs.reeva.runtime.Operations
 import com.reevajs.reeva.runtime.objects.JSObject
+import com.reevajs.reeva.runtime.primitives.JSUndefined
+import com.reevajs.reeva.utils.unreachable
 
-class JSGeneratorObject(
+class JSGeneratorObject private constructor(
     realm: Realm,
-    val interpreter: Interpreter,
-    val envRecord: EnvRecord,
+    val receiver: JSValue,
+    arguments: List<JSValue>,
+    val executable: Executable,
+    val generatorState: Interpreter.GeneratorState,
+    var envRecord: EnvRecord,
 ) : JSObject(realm, realm.generatorObjectProto) {
-    // TODO: Generators
-    // fun next(realm: Realm, mode: Interpreter.SuspendedEntryMode, value: JSValue): JSValue {
-    //     return when (val result = interpreter.reenterSuspendedFunction(mode, value)) {
-    //         null -> Operations.createIterResultObject(realm, JSUndefined, true)
-    //         is ExecutionResult.Success -> Operations.createIterResultObject(realm, result.value, false)
-    //         else -> {
-    //             expect(result is ExecutionResult.RuntimeError)
-    //             throw ThrowException(result.value)
-    //         }
-    //     }
-    // }
+    private val arguments = listOf(receiver, JSUndefined, generatorState) + arguments
+
+    fun next(realm: Realm, value: JSValue): JSValue {
+        generatorState.sentValue = value
+        return execute(realm)
+    }
+
+    fun `return`(realm: Realm, value: JSValue): JSValue {
+        TODO()
+    }
+
+    fun `throw`(realm: Realm, value: JSValue): JSValue {
+        TODO()
+    }
+
+    private fun execute(realm: Realm): JSValue {
+        val interpreter = Interpreter(realm, executable, arguments, envRecord)
+        when (val result = interpreter.interpret()) {
+            is ExecutionResult.Success -> {
+                envRecord = interpreter.activeEnvRecord
+                return Operations.createIterResultObject(
+                    realm,
+                    result.value,
+                    generatorState.phase == -1,
+                )
+            }
+            is ExecutionResult.InternalError -> throw result.cause
+            is ExecutionResult.RuntimeError -> throw ThrowException(result.value)
+            is ExecutionResult.ParseError -> unreachable()
+        }
+    }
 
     companion object {
-        fun create(realm: Realm, interpreter: Interpreter, envRecord: EnvRecord) =
-            JSGeneratorObject(realm, interpreter, envRecord).initialize()
+        fun create(
+            realm: Realm,
+            receiver: JSValue,
+            arguments: List<JSValue>,
+            executable: Executable,
+            generatorState: Interpreter.GeneratorState,
+            envRecord: EnvRecord,
+        ) = JSGeneratorObject(realm, receiver, arguments, executable, generatorState, envRecord).initialize()
     }
 }
