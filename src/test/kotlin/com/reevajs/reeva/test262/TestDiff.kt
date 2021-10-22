c840b34f04ce3436c1c245e0211993bf6cf7c3d1
Matthew Olsson <matthewcolsson@gmail.com>
2021-10-22 14:13:32 -0700

    TestDiff: Improve output

--- src/test/kotlin/com/reevajs/reeva/test262/TestDiff.kt
+++ src/test/kotlin/com/reevajs/reeva/test262/TestDiff.kt
@@ -1,5 +1,6 @@
 package com.reevajs.reeva.test262
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import java.io.File
@@ -11,20 +12,30 @@ fun main() {
     val oldJson = File("demo/test_results", OLD_NAME).readText()
     val newJson = File("demo/test_results", NEW_NAME).readText()
 
-    val json = Json { }
-    val oldContents = json.decodeFromString<List<Test262Runner.TestResult>>(oldJson)
-    val newContents = json.decodeFromString<List<Test262Runner.TestResult>>(newJson)
+    val oldContents = Json.decodeFromString<List<Test262Runner.TestResult>>(oldJson)
+    val newContents = Json.decodeFromString<List<Test262Runner.TestResult>>(newJson)
 
     val newMap = newContents.associateBy { it.name }
 
+    val newPass = mutableListOf<String>()
+    val newFail = mutableListOf<String>()
+
     for (test in oldContents) {
         val newTest = newMap[test.name]!!
+        val wasSuccess = test.status == Test262Runner.TestResult.Status.Passed
+        val isSuccess = newTest.status == Test262Runner.TestResult.Status.Passed
 
-        if (test.status == Test262Runner.TestResult.Status.Passed &&
-            newTest.status != Test262Runner.TestResult.Status.Passed
-        ) {
+        if (wasSuccess != isSuccess) {
             val file = File(Test262Runner.testDirectory, test.name)
-            println(file.absolutePath)
+            (if (isSuccess) newPass else newFail).add(file.absolutePath)
         }
     }
+
+    println("New passes: ${newPass.size}, new fails: ${newFail.size}")
+
+    for (path in newPass)
+        println("+ $path")
+
+    for (path in newFail)
+        println("- $path")
 }
